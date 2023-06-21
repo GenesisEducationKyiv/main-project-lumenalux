a556f109a192576414a2debac73b45464b9a395f
Stefan Martyniuk <stefanmartinuk@gmail.com>
2023-06-21 18:21:29 +0300

    Rewrite the config tests

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -1,60 +1,89 @@
 package config
 
 import (
+	"io"
 	"os"
-	"reflect"
 	"testing"
-
-	yaml "gopkg.in/yaml.v2"
 )
 
 func TestLoad(t *testing.T) {
-	sampleConfig := Config{
-		Email: EmailConfig{
-			From:    "test@example.com",
-			Subject: "test subject",
-			Body:    "test body",
-		},
-		SMTP: SMTPConfig{
-			Host:     "smtp.example.com",
-			Port:     587,
-			User:     "user",
-			Password: "password",
-		},
-		Storage: StorageConfig{
-			Path: "/tmp",
-		},
-		HTTP: HTTPConfig{
-			Port: "8080",
-		},
-		KunaAPI: KunaAPIConfig{
-			URL: "https://kuna.io/api/v2",
+	testData := []struct {
+		name           string
+		yaml           string
+		expectedConfig Config
+	}{
+		{
+			name: "ValidConfig",
+			yaml: `
+smtp:
+  host: smtp.example.com
+  port: 587
+  user: user@example.com
+  password: secret
+
+email:
+  from: user@example.com
+  subject: Test Subject
+  body: Test Body
+
+storage:
+  path: /var/data
+
+http:
+  port: "8080"
+  timeout_in_seconds: 10
+
+kuna_api:
+  url: https://api.example.com
+`,
+			expectedConfig: Config{
+				SMTP: SMTPConfig{
+					Host:     "smtp.example.com",
+					Port:     587,
+					User:     "user@example.com",
+					Password: "secret",
+				},
+				Email: EmailConfig{
+					From:    "user@example.com",
+					Subject: "Test Subject",
+					Body:    "Test Body",
+				},
+				Storage: StorageConfig{
+					Path: "/var/data",
+				},
+				HTTP: HTTPConfig{
+					Port:    "8080",
+					Timeout: 10,
+				},
+				KunaAPI: KunaAPIConfig{
+					URL: "https://api.example.com",
+				},
+			},
 		},
 	}
 
-	data, err := yaml.Marshal(&sampleConfig)
-	if err != nil {
-		t.Fatalf("failed to marshal sample config: %v", err)
-	}
+	for _, testData := range testData {
+		t.Run(testData.name, func(t *testing.T) {
 
-	tmpfile, err := os.CreateTemp("", "testconfig")
-	if err != nil {
-		t.Fatalf("failed to create temporary file: %v", err)
-	}
-	defer os.Remove(tmpfile.Name())
+			tempFile, err := os.CreateTemp("", "config_test")
+			if err != nil {
+				t.Fatalf("Failed to create temporary file: %v", err)
+			}
+			defer os.Remove(tempFile.Name())
 
-	if _, err := tmpfile.Write(data); err != nil {
-		t.Fatalf("failed to write to temporary file: %v", err)
-	}
-	if err := tmpfile.Close(); err != nil {
-		t.Fatalf("failed to close temporary file: %v", err)
-	}
+			_, err = io.WriteString(tempFile, testData.yaml)
+			if err != nil {
+				t.Fatalf("Failed to write test data to the temporary file: %v", err)
+			}
 
-	if err := Load(tmpfile.Name()); err != nil {
-		t.Fatalf("failed to load config: %v", err)
-	}
+			config, err := Load(tempFile.Name())
+			if err != nil {
+				t.Fatalf("Failed to load configuration: %v", err)
+			}
 
-	if !reflect.DeepEqual(Current(), sampleConfig) {
-		t.Errorf("loaded config does not match expected config")
+			if config != testData.expectedConfig {
+				t.Errorf("Unexpected configuration. Got: %+v, Expected: %+v", config, testData.expectedConfig)
+			}
+		})
 	}
 }
