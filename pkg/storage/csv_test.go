b806f85029e881ea8a8250f914f71b9ef7deb3e4
Stefan Martyniuk <stefanmartinuk@gmail.com>
2023-06-28 16:31:13 +0300

    Split csv storage test

--- pkg/storage/csv_test.go
+++ pkg/storage/csv_test.go
@@ -2,32 +2,54 @@ package storage
 
 import (
 	"os"
-	"reflect"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
-func TestCSVStorage(t *testing.T) {
+func setup(t *testing.T) (*CSVStorage, func()) {
 	tmpfile, err := os.CreateTemp("", "example")
 	if err != nil {
 		t.Fatalf("failed to create temporary file: %v", err)
 	}
 
-	defer os.Remove(tmpfile.Name())
-
 	storage := NewCSVStorage(tmpfile.Name())
+
+	return storage, func() {
+		os.Remove(tmpfile.Name())
+	}
+}
+
+func TestCSVStorageAppend(t *testing.T) {
+	storage, teardown := setup(t)
+	defer teardown()
 
 	data := "example@test.com"
 
+	t.Run("Append data to storage", func(t *testing.T) {
+		if err := storage.Append(data); err != nil {
+			t.Fatalf("failed to append data: %v", err)
+		}
+	})
+}
+
+func TestCSVStorageAllRecords(t *testing.T) {
+	storage, teardown := setup(t)
+	defer teardown()
+
+	data := "example@test.com"
 	if err := storage.Append(data); err != nil {
 		t.Fatalf("failed to append data: %v", err)
 	}
 
-	readData, err := storage.AllRecords()
-	if err != nil {
-		t.Fatalf("failed to read data: %v", err)
-	}
+	t.Run("Read data from storage", func(t *testing.T) {
+		readData, err := storage.AllRecords()
+		if err != nil {
+			t.Fatalf("failed to read data: %v", err)
+		}
 
-	if !reflect.DeepEqual(readData[0][0], data) {
-		t.Errorf("read data does not match written data: got %v, want %v", readData[0][0], data)
-	}
+		if diff := cmp.Diff(data, readData[0][0]); diff != "" {
+			t.Errorf("read data does not match written data (-want +got):\n%s", diff)
+		}
+	})
 }
