b86fd0d93683bf8b15b772effd0c6401a058449b
Stefan Martyniuk <stefanmartinuk@gmail.com>
2023-06-28 13:29:50 +0300

    Combine test functions into a table-driven test

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -10,19 +10,22 @@ import (
 func TestLoad(t *testing.T) {
 	testData := []struct {
 		name           string
-		yaml           string
-		expectedConfig Config
+		yamlName       string
+		yamlContent    string
+		expectedConfig *Config
+		expectedErr    error
 	}{
 		{
-			name: "Load SMTP config",
-			yaml: `
+			name:     "Load SMTP config",
+			yamlName: "test_config.yaml",
+			yamlContent: `
 smtp:
   host: smtp.example.com
   port: 587
   user: user@example.com
   password: secret
 `,
-			expectedConfig: Config{
+			expectedConfig: &Config{
 				SMTP: SMTPConfig{
 					Host:     "smtp.example.com",
 					Port:     587,
@@ -32,14 +35,15 @@ smtp:
 			},
 		},
 		{
-			name: "Load email config",
-			yaml: `
+			name:     "Load email config",
+			yamlName: "test_config.yaml",
+			yamlContent: `
 email:
   from: user@example.com
   subject: Test Subject
   body: Test Body
 `,
-			expectedConfig: Config{
+			expectedConfig: &Config{
 				Email: EmailConfig{
 					From:    "user@example.com",
 					Subject: "Test Subject",
@@ -48,25 +52,27 @@ email:
 			},
 		},
 		{
-			name: "Load storage config",
-			yaml: `
+			name:     "Load storage config",
+			yamlName: "test_config.yaml",
+			yamlContent: `
 storage:
   path: /var/data
 `,
-			expectedConfig: Config{
+			expectedConfig: &Config{
 				Storage: StorageConfig{
 					Path: "/var/data",
 				},
 			},
 		},
 		{
-			name: "Load HTTP config",
-			yaml: `
+			name:     "Load HTTP config",
+			yamlName: "test_config.yaml",
+			yamlContent: `
 http:
   port: "8080"
   timeout_in_seconds: 10
 `,
-			expectedConfig: Config{
+			expectedConfig: &Config{
 				HTTP: HTTPConfig{
 					Port:    "8080",
 					Timeout: 10,
@@ -74,66 +80,84 @@ http:
 			},
 		},
 		{
-			name: "Load Kuna API config",
-			yaml: `
+			name:     "Load Kuna API config",
+			yamlName: "test_config.yaml",
+			yamlContent: `
 kuna_api:
   url: https://api.example.com
 `,
-			expectedConfig: Config{
+			expectedConfig: &Config{
 				KunaAPI: KunaAPIConfig{
 					URL: "https://api.example.com",
 				},
 			},
 		},
+		{
+			name:        "Non existent file",
+			yamlName:    "nonexistentfile.yaml",
+			expectedErr: ErrReadFile,
+		},
+		{
+			name:        "Invalid YAML",
+			yamlName:    "config_test",
+			yamlContent: `:\C`,
+			expectedErr: ErrUnmarshalYAML,
+		},
 	}
 
-	for _, testData := range testData {
-		t.Run(testData.name, func(t *testing.T) {
-
-			tempFile, err := os.CreateTemp("", "config_test")
-			if err != nil {
-				t.Fatalf("Failed to create temporary file: %v", err)
-			}
-			defer os.Remove(tempFile.Name())
-
-			_, err = io.WriteString(tempFile, testData.yaml)
-			if err != nil {
-				t.Fatalf("Failed to write test data to the temporary file: %v", err)
-			}
-
-			config, err := Load(tempFile.Name())
-			if err != nil {
-				t.Fatalf("Failed to load configuration: %v", err)
-			}
-
-			if config != testData.expectedConfig {
-				t.Errorf("Unexpected configuration. Got: %+v, Expected: %+v", config, testData.expectedConfig)
-			}
+	for _, td := range testData {
+		t.Run(td.name, func(t *testing.T) {
+			runTest(t, td)
 		})
 	}
 }
 
-func TestLoadNonExistentFile(t *testing.T) {
-	_, err := Load("nonexistentfile.yaml")
-	if err == nil || !errors.Is(err, ErrReadFile) {
-		t.Fatalf("Expected error %v but got %v", ErrReadFile, err)
+func runTest(t *testing.T, td struct {
+	name           string
+	yamlName       string
+	yamlContent    string
+	expectedConfig *Config
+	expectedErr    error
+}) {
+	var fileName string
+
+	if td.expectedErr != nil && errors.Is(td.expectedErr, ErrReadFile) {
+		fileName = td.yamlName
+	} else {
+		fileName = prepareTempFile(t, td.yamlName, td.yamlContent)
+		defer os.Remove(fileName)
 	}
+
+	config, err := Load(fileName)
+	compareResults(t, &config, err, td.expectedConfig, td.expectedErr)
 }
 
-func TestLoadInvalidYAML(t *testing.T) {
-	tempFile, err := os.CreateTemp("", "config_test")
+func prepareTempFile(t *testing.T, yamlName string, yamlContent string) string {
+	tempFile, err := os.CreateTemp("", yamlName)
 	if err != nil {
 		t.Fatalf("Failed to create temporary file: %v", err)
 	}
-	defer os.Remove(tempFile.Name())
 
-	_, err = io.WriteString(tempFile, `:\C`)
+	_, err = io.WriteString(tempFile, yamlContent)
 	if err != nil {
 		t.Fatalf("Failed to write test data to the temporary file: %v", err)
 	}
 
-	_, err = Load(tempFile.Name())
-	if err == nil || !errors.Is(err, ErrUnmarshalYAML) {
-		t.Fatalf("Expected error %v but got %v", ErrUnmarshalYAML, err)
+	return tempFile.Name()
+}
+
+func compareResults(
+	t *testing.T,
+	config *Config,
+	err error,
+	expectedConfig *Config,
+	expectedErr error,
+) {
+	if !errors.Is(err, expectedErr) {
+		t.Fatalf("Expected error %v but got %v", expectedErr, err)
+	}
+
+	if expectedConfig != nil && *config != *expectedConfig {
+		t.Errorf("Unexpected configuration. Got: %+v, Expected: %+v", *config, *expectedConfig)
 	}
 }
