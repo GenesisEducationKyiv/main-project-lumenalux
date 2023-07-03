1bc765cf8792fa5321f2eaa33b27e947a5ff4c04
Stefan Martyniuk <stefanmartinuk@gmail.com>
2023-07-03 14:30:05 +0300

    Refactor service test
    Rewriten using testify/require
    Added t.Parallel() to service test

--- internal/subscription/service_test.go
+++ internal/subscription/service_test.go
@@ -1,8 +1,9 @@
 package subscription
 
 import (
-	"errors"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type StubStorage struct {
@@ -21,50 +22,59 @@ func (m *StubStorage) AllRecords() ([][]string, error) {
 
 func TestSubscription(t *testing.T) {
 	t.Run("Subscribe and check subscriptions", func(t *testing.T) {
+		t.Parallel()
+
 		stubStorage := &StubStorage{Records: [][]string{}}
 		service := NewService(stubStorage)
 		email := "test@example.com"
 
 		err := service.Subscribe(email)
-		if err != nil {
-			t.Errorf("unexpected error: %v", err)
-		}
+		require.NoError(t, err)
 
 		subscribed, err := service.IsSubscribed(email)
-		if err != nil || !subscribed {
-			t.Errorf("expected email to be subscribed, got: %v", err)
-		}
+		require.NoError(t, err)
+		require.True(t, subscribed, "expected email to be subscribed")
 
 		subscriptions, err := service.Subscriptions()
-		if err != nil || len(subscriptions) != 1 || subscriptions[0] != email {
-			t.Errorf("expected subscription list to contain email, got: %v", subscriptions)
-		}
+		require.NoError(t, err)
+
+		require.Equal(
+			t, 1, len(subscriptions),
+			"expected subscription list to contain one email",
+		)
+
+		require.Equal(
+			t, email, subscriptions[0],
+			"expected subscription list to contain the email",
+		)
 	})
 
 	t.Run("Subscribe twice", func(t *testing.T) {
+		t.Parallel()
+
 		stubStorage := &StubStorage{Records: [][]string{}}
 		service := NewService(stubStorage)
 		email := "test@example.com"
 
 		err := service.Subscribe(email)
-		if err != nil {
-			t.Errorf("unexpected error: %v", err)
-		}
+		require.NoError(t, err)
 
 		err = service.Subscribe(email)
-		if !errors.Is(err, ErrAlreadySubscribed) {
-			t.Errorf("expected error due to duplicate subscription, got: %v", err)
-		}
+		require.ErrorIs(
+			t, err, ErrAlreadySubscribed,
+			"expected error due to duplicate subscription",
+		)
 	})
 
 	t.Run("Subscribed with non-existent email", func(t *testing.T) {
-		mockStorage := &StubStorage{Records: [][]string{}}
-		service := NewService(mockStorage)
+		t.Parallel()
+
+		stubStorage := &StubStorage{Records: [][]string{}}
+		service := NewService(stubStorage)
 		email := "test@example.com"
 
 		subscribed, err := service.IsSubscribed(email)
-		if err != nil || subscribed {
-			t.Errorf("expected email not to be subscribed, got: %v", err)
-		}
+		require.NoError(t, err)
+		require.False(t, subscribed, "expected email not to be subscribed")
 	})
 }
